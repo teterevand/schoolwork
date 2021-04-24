deabf8397651e791b38e2067ba329a082faaf5ec
Andrey Teterev <ateterev@ncomputing.com>
2021-04-24 10:08:24 +0300

    First application

---
+++ SwapDigits/SwapDigits/SwapDigits.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	int x = 20;
+	int y = 30;
+
+	std::cout << "First number: " << x << std::endl;
+	std::cout << "Second number: " << y << std::endl;
+
+	std::cout << "Swapping...\n";
+	
+	int tmp = x;
+	x = y;
+	y = tmp;
+
+	std::cout << "First number: " << x << std::endl;
+	std::cout << "Second number: " << y << std::endl;
+
+	return 0;
+}
